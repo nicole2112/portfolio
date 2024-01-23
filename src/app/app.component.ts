bc66264a775d5de23d88114cd4473a14271248c4
nicole2112 <nicole.rodriguez@unitec.edu>
2024-01-22 21:30:20 -0600

    footer and fontawesome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, NavbarComponent, HomeComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, NavbarComponent, HomeComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPhone, faComments, faEnvelope, faMapMarker } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [FontAwesomeModule],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+  faPhone = faPhone;
+  faComments = faComments;
+  faEnvelope = faEnvelope;
+  faMapMarker = faMapMarker;
+  faLinkedin = faLinkedinIn;
+}
