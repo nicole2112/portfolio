2c35eedf39643dad15f6b55e52d1e8fdffcd755d
nicole2112 <nicole.rodriguez@unitec.edu>
2024-01-29 21:16:17 -0600

    fundacion padrino project

--- src/app/core/ProjectsData.ts
+++ src/app/core/ProjectsData.ts
@@ -15,9 +15,20 @@ const chartProjectImages: Array<ProjectImage> = [
     { name: 'Screen 3: Image Validation', sourceUrl: '/assets/images-projects/chart2023/img4.png' },
 ]
 
+const fundacionProjectImages: Array<ProjectImage> = [
+    { name: 'Landing page', sourceUrl: '/assets/images-projects/fundaPadrino/img1.png' },
+    { name: 'Landing page: Testimony', sourceUrl: '/assets/images-projects/fundaPadrino/img2.png' },
+    { name: 'Landing page: Contact', sourceUrl: '/assets/images-projects/fundaPadrino/img3.png' },
+    { name: 'Landing page: Blog', sourceUrl: '/assets/images-projects/fundaPadrino/img4.png' },
+    { name: 'Administrative login page', sourceUrl: '/assets/images-projects/fundaPadrino/img5.png' },
+    { name: 'Administrative heroes section management', sourceUrl: '/assets/images-projects/fundaPadrino/img6.png' },
+    { name: 'Administrative heroes sectio management', sourceUrl: '/assets/images-projects/fundaPadrino/img8.png' },
+    { name: 'Heroes Section', sourceUrl: '/assets/images-projects/fundaPadrino/img7.png' },
+]
+
 export const projectsData: Array<Project> = [
     { id: 1, order:1, hidden:false, title: 'Chart Classification', codeUrl: 'https://github.com/nicole2112/CHART2023', prodUrl: 'https://chart2023project.web.app/', images: chartProjectImages, description: 'With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.'},
-    { id: 2, title: 'Fundación Padrino', codeUrl: 'https://github.com/nicole2112/Proyecto_ISW', prodUrl:"https://fundacionpadrino.org/pagina-principal", images: images, description: 'With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.'},
+    { id: 2, order:2, title: 'Fundación Padrino', codeUrl: 'https://github.com/nicole2112/Proyecto_ISW', prodUrl:"https://fundacionpadrino.org/pagina-principal", images: fundacionProjectImages, description: 'With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.'},
     { id: 3, title: 'Project C', prodUrl: 'https://github.com/nicole2112/CHART2023', images: images, description: 'With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.'},
     { id: 4, title: 'Project D', images: images, description: 'With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.'},
     { id: 5, title: 'Project E', codeUrl: 'https://github.com/nicole2112/CHART2023', images: images, description: 'With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.'},
